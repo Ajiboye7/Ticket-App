2b4fc677f09460278041e1079596f4302dbf25ef
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-06-21 12:59:05 +0100

    update

--- app/ticket/create/CreateForm.jsx
+++ app/ticket/create/CreateForm.jsx
@@ -11,9 +11,61 @@ export default function CreateForm() {
     const [body, setBody] = useState('')
     const [priority, setPriority] = useState('low')
     const [isLoading, setIsLoading] = useState(false)
+
+    const handleSubmit =async(e)=>{
+        e.preventDefault()
+        setIsLoading(true)
+        const ticket ={
+            title, body, priority , user_email : "ajiboyemuyideen7@gmail.com"
+        }
+        const res = await fetch ("http://localhost:4000/tickets", {
+            method: "POST",
+            headers: {"content-Type": "application/json"},
+            body: JSON.stringify(ticket)
+        })
+        if (res.status === 201){
+            router.push('/ticket')
+        }
+    }
+
+    
   return (
-    <form className="w-1/2">
-      
-    </form>
+    <form onSubmit ={handleSubmit}className="w-1/2">
+    <label>
+      <span>Title:</span>
+      <input required 
+      type="text"
+      name="title" 
+      onChange={(e)=> setTitle(e.target.value)}
+      value={title}
+
+      />
+    </label>
+    <label>
+      <span>Body:</span>
+      <textarea
+      required 
+      name="body"
+      onChange={(e)=> setBody(e.target.value)}
+      value={body} />
+    </label>
+    <label>
+      <span>Priority:</span>
+      <select 
+      name="priority"
+      onChange={(e)=> setPriority(e.target.value)}
+      value={priority}>
+        <option value="low">Low Priority</option>
+        <option value="medium">Medium Priority</option>
+        <option value="high">High Priority</option>
+      </select>
+    </label>
+   <button className="btn-primary"
+   disabled = {isLoading}
+   >
+   {isLoading && <span>Adding...</span>}
+   {!isLoading && <span>Add Ticket</span>}
+   </button>
+  </form>
   )
 }
