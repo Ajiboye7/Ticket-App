e9f34c6041af89d73161115182956f065b8a5f62
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-06-22 15:33:11 +0100

    latest

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <h1>My Helpdesk</h1>
       <Link href='/'>Dashboard</Link>
       <Link href='ticket'>Tickets</Link>
-      <Link href='create'>Create</Link>
+      <Link href='ticket/create'>Create</Link>
     </nav>
   )
 }
